e8d93576cdd56394158c4a7c0bda304c3f33abed
unknown <bruno.giannellam@gmail.com>
2019-10-27 00:11:27 -0300

    Gateway

--- zuulsvr/src/main/java/br/com/fiap/ecommerce/zuul/ZuulServerApplication.java
+++ zuulsvr/src/main/java/br/com/fiap/ecommerce/zuul/ZuulServerApplication.java
@@ -3,7 +3,13 @@ package br.com.fiap.ecommerce.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import br.com.fiap.ecommerce.zuul.filters.ErrorFilter;
+import br.com.fiap.ecommerce.zuul.filters.PostFilter;
+import br.com.fiap.ecommerce.zuul.filters.PreFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,4 +18,19 @@ public class ZuulServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
+    
+    @Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
 }
